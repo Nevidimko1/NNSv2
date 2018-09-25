ba134609734525ff32ab0e20fa1c58028ce2c1e4
Sergey Mankevich <s.mankevics@gmail.com>
2018-09-25 15:01:59 +0300

    bootstrap app process updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,18 +17,35 @@ import { AppComponent } from './app.component';
 })
 export class AppModule {
   private unitListMain: boolean;
-  private unitList: boolean;
   private nns: boolean;
 
   constructor() {
     const location = window.location.href.match(/view\/(\d+)(\/[a-zA-Z_]+)?(\/[a-zA-Z_]+)?/);
 
-    this.unitListMain = location && (!location[2] || location[2] === '/unit_list'),
+    this.unitListMain = location && (!location[2] || location[2] === '/unit_list');
     this.nns = this.unitListMain && location[3] === '/nns';
+
+    if (this.unitListMain) {
+      const tabs = document.querySelectorAll('.tabu li') as any as HTMLElement[];
+      const nnsTab = tabs[0].cloneNode(true) as HTMLElement;
+      nnsTab.querySelector('a').href += '/nns';
+      nnsTab.querySelector('a').textContent = 'NNS';
+      nnsTab.classList.add('nns-tab');
+      nnsTab.classList.remove('sel');
+      document.querySelector('.tabu').appendChild(nnsTab);
+
+      if (this.nns) {
+        // remove selected tab classes
+        tabs.forEach(el => el.classList.remove('sel'));
+        // keep nns tab selected
+        nnsTab.classList.add('sel');
+      }
+    }
   }
+
   ngDoBootstrap(app) {
     if (!this.nns) {
-      // run app only on NNS script page
+      // bootstrap only on NNS script page
       return;
     }
 
